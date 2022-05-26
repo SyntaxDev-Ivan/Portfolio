b2e98c414f1cba2166a1eb097f4295f2b8a141e0
Ivan Opacak <sandrostross310@gmail.com>
2022-05-26 16:56:29 +0200

    initial commit for portfolio-webpage

--- Portfolio-Webpage/karma.conf.js
+++ Portfolio-Webpage/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Portfolio-Webpage'),
+      dir: require('path').join(__dirname, './coverage/portfolio-webpage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
