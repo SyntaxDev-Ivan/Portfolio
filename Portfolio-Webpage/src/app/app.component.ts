7c1ddfaeee91bf5d830642a0ca85ff9e517f7c88
Ivan Opacak <sandrostross310@gmail.com>
2022-05-28 19:12:07 +0200

    sidebar fertig!

--- Portfolio-Webpage/src/app/app.component.ts
+++ Portfolio-Webpage/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio-Webpage';
+
+  ngClasses = {
+    home: {
+      active: false
+    },
+    portfolio: {
+      active: false
+    },
+    ich: {
+      active: false
+    }
+  }
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+    switch (this.router.url) {
+      case '/':
+        this.router.navigate(['/home'])
+        this.deactivateActiveElements()
+        this.ngClasses.home.active = true
+        break
+      case '/portfolio':
+        this.deactivateActiveElements()
+        this.ngClasses.portfolio.active = true
+        break
+      case '/ich':
+        this.deactivateActiveElements()
+        this.ngClasses.ich.active = true
+        break
+      default:
+        this.deactivateActiveElements()
+
+    }
+  }
+
+  deactivateActiveElements(): void {
+    this.ngClasses.home.active = false
+    this.ngClasses.portfolio.active = false
+    this.ngClasses.ich.active = false
+  }
+
+  redirectToGithub() {
+    window.open('https://github.com/ivopacak/Portfolio', '_blank')
+  }
 }
