592e61ad46c2da238aefb742c9684076aebb1896
Ivan Opacak <sandrostross310@gmail.com>
2022-06-09 19:49:31 +0200

    production update 1.0

--- ivan-opacak.me/src/app/app-routing.module.ts
+++ ivan-opacak.me/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IchComponent } from './ich/ich.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'portfolio', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) },
+  { path: 'portfolio', component: PortfolioComponent },
   { path: 'ich', component: IchComponent },
 ];
 
