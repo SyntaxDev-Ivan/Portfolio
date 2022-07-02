399502e7e278727246891cd000cc9fff1a76d7b4
Ivan Opacak <sandrostross310@gmail.com>
2022-07-02 12:19:15 +0200

    initial commit

---
+++ Frontend-Projekte/Angular/Weather-App/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { LocationComponent } from './location/location.component';
+import { WeatherComponent } from './weather/weather.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchBarComponent,
+    LocationComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
